1fe51982f27f53cfa15a39ac145f9fb01c4a8bd5
zhoupengcheng03 <zhoupengcheng03@corp.netease.com>
2021-12-10 11:17:17 +0800

    Go 归并排序

--- 排序/归并排序.go
+++ 排序/归并排序.go
@@ -1,1 +1,28 @@
 package 排序
+
+func mergeSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := mergeSort(nums[:mid])  //左边归并排序，使得左子序列有序
+	right := mergeSort(nums[mid:]) //右边归并排序，使得右子序列有序
+	return merge(left, right)      //将两个有序子数组合并操作
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
